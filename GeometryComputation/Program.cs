1b05ae3413c9fd117a715c49da905190c283ad89
challa ramakrishna reddy <114689103+challaramakrishnareddy52@users.noreply.github.com>
2022-11-25 22:11:37 +0530

    Update Program.cs

--- GeometryComputation/Program.cs
+++ GeometryComputation/Program.cs
@@ -54,10 +54,8 @@
             Console.WriteLine("Found Line 2 Length Value :{Line2Distance}");
             Console.WriteLine();
             Console.WriteLine($"So Finally get Line 1 Length Value :{Line1Distance} & Line 2 Length Value :{Line2Distance}");
-
             Console.Write("Using Equals Method output is :::: ");
             GeometryLineComputation.EqualsMethod(Line1Distance, Line2Distance);
-
             Console.WriteLine("Using Compare Method output is ::::");
             GeometryLineComputation.CompareMethod(Line1Distance, Line2Distance);
 
